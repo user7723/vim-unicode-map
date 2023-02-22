42ec718cc4da1d62051e6e39d0ceeaf29ed2b418
user7723 <99251541+user7723@users.noreply.github.com>
2023-02-22 21:54:55 +0300

    key mappings edit

--- plugin/tblgen.sh
+++ plugin/tblgen.sh
@@ -13,15 +13,34 @@ function escape() {
   perl -pe 's/(\[|\]|~)/\\\1/g' # [|] -> \[ | \]
 }
 
+# map from keyword to unicode
+# k -> u
 MAPTO='TXTtoUTF'
+
+# map from unicode to keyword
+# u -> k
 MAPFROM='UTFtoTXT'
 
-printf 'nnoremap <leader>k :call  '$MAPFROM'(1,line("$"))<CR>\n'
+# u -> k is done linewise by default (because we probably want to translate
+#     only those chars that appear to be on a line we are currently editing
+#     but not on the entire document)
+printf 'nnoremap <leader>k :call '$MAPFROM'(line("."),line("."))<CR>\n\n'
+
+# u -> k is linewise in visual mode
 printf 'vnoremap <leader>k :call  '$MAPFROM'(line("'\''<"),line("'\''>"))<CR>\n'
+
+# u -> k is global if 'g' is prefixed
+printf 'nnoremap <leader>gk :call  '$MAPFROM'(1,line("$"))<CR>\n'
+
+# k -> u is global by default
 printf 'nnoremap <leader>u :call  '$MAPTO'(1,line("$"))<CR>\n'
+
+# k -> u is linewise in visual mode
 printf 'vnoremap <leader>u :call  '$MAPTO'(line("'\''<"),line("'\''>"))<CR>\n'
+
+# k -> u is linwise if 'l' is prefixed
 printf 'nnoremap <leader>lu :call '$MAPTO'(line("."),line("."))<CR>\n'
-printf 'nnoremap <leader>lk :call '$MAPFROM'(line("."),line("."))<CR>\n\n'
+
 
 printf 'function! '$MAPFROM'(begin,end)
   let s:line = line(".")
